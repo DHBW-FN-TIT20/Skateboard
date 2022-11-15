91ea3fae0ee23702eec3acb9ddc69636f9c9a45c
LukasAdrion <Lukas@Adrion.com>
2022-11-16 00:53:01 +0100

    :bulb: add comments to camera.js and light.js

--- World/components/camera.js
+++ World/components/camera.js
@@ -2,10 +2,10 @@ import { PerspectiveCamera } from "three";
 
 function createCamera(){
   const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    1000
+    75,  //field of view
+    window.innerWidth / window.innerHeight, 
+    0.01, //near
+    1000 //far
   );
   camera.position.set(0,15,-10);
   return camera;
