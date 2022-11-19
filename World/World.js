b9d5f3408a4f2af098a3c8b2ec184b40c993e8a1
LukasAdrion <Lukas@Adrion.com>
2022-11-19 17:03:25 +0100

    :sparkles: add letterboxes with hitbox

--- World/World.js
+++ World/World.js
@@ -27,6 +27,7 @@ import { loadBush } from "./components/environment/bush";
 import { loadBank } from "./components/environment/bank";
 import { loadTrashcan } from "./components/environment/trashcan";
 import { loadChairTable } from "./components/environment/chair-table";
+import { loadLetterBox } from "./components/environment/letter-box";
 
 let scene;
 let physics;
@@ -90,9 +91,9 @@ class World {
         const environment = await loadEnvironment();
         const skateboard = await loadSkateboard();
         const hydrant = await loadHydrant();
-        const streetLamp1 = await loadStreetLamp(-19, .1, 0);
-        const streetLamp2 = await loadStreetLamp(-19, .1, 15);
-        const streetLamp3 = await loadStreetLamp(-19, .1, -15);
+        const streetLamp1 = await loadStreetLamp(-19.4, .1, 0);
+        const streetLamp2 = await loadStreetLamp(-19.4, .1, 15);
+        const streetLamp3 = await loadStreetLamp(-19.4, .1, -15);
         const pylon1 = await loadPylon(-2, .1, -10);
         const pylon2 = await loadPylon(4, .1, -10);
         const pylon3 = await loadPylon(-8, .1, -10);
@@ -109,13 +110,15 @@ class World {
         const bank = await loadBank(12, 0, 17);
         const trashcan1 = await loadTrashcan(8, 2, 17);
         const chariTable = await loadChairTable();
+        const letterBox1 = await loadLetterBox(-19.7, .1, 7.5);
+        const letterBox2 = await loadLetterBox(-19.7, .1, -7.5);
     
         skateboard.physics.addToWorld(physics);
         
         loop.updatables.push(skateboard.model, pylon1.model, pylon2.model, pylon3.model, pylon4.model, pylon5.model ,pylon6.model, pylon7.model ,trashcan1.model);
-        scene.add(skateboard.model, environment.model, hydrant.model, streetLamp1.model, streetLamp2.model, streetLamp3.model, street, tree.model, pylon1.model, prism.model, pylon2.model, pylon3.model, bush1, bush2, bank.model, trashcan1.model, chariTable.model, pylon5.model, pylon4.model, pylon6.model, pylon7.model);
+        scene.add(skateboard.model, environment.model, hydrant.model, streetLamp1.model, streetLamp2.model, streetLamp3.model, street, tree.model, pylon1.model, prism.model, pylon2.model, pylon3.model, bush1, bush2, bank.model, trashcan1.model, chariTable.model, pylon5.model, pylon4.model, pylon6.model, pylon7.model, letterBox1.model, letterBox2.model);
         scene.add(streetLamp1.spotLightLamp, streetLamp1.spotLightLamp.target, streetLamp2.spotLightLamp, streetLamp2.spotLightLamp.target ,streetLamp3.spotLightLamp, streetLamp3.spotLightLamp.target, car.model);
-        [environment.physics, hydrant.physics, streetLamp1.physics, streetLamp2.physics, streetLamp3.physics, pylon1.physics, prism.physics, pylon2.physics, pylon3.physics, car.physics, bank.physics, tree.physics, trashcan1.physics, chariTable.physics, pylon4.physics, pylon5.physics, pylon6.physics, pylon7.physics].forEach((p) => physics.addBody(p));
+        [environment.physics, hydrant.physics, streetLamp1.physics, streetLamp2.physics, streetLamp3.physics, pylon1.physics, prism.physics, pylon2.physics, pylon3.physics, car.physics, bank.physics, tree.physics, trashcan1.physics, chariTable.physics, pylon4.physics, pylon5.physics, pylon6.physics, pylon7.physics, letterBox1.physics, letterBox2.physics].forEach((p) => physics.addBody(p));
     }
 
     render() {
---
+++ World/components/environment/letter-box.js
@@ -0,0 +1,30 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as CANNON from "cannon-es";
+import { degToRad } from "three/src/math/MathUtils";
+
+async function loadLetterBox(xPos, yPos, zPos){
+  const gltfLoader = new GLTFLoader();
+  const data = await gltfLoader.loadAsync("/models/environment/letter-box.glb");
+  const model = data.scene.children[0];
+
+  model.scale.set(.1,.1,.1);
+  model.position.set(xPos,yPos,zPos);
+  model.rotateY(degToRad(90));
+  
+  model.castShadow = true;
+  data.scene.traverse((node) => {
+    if (node.isMesh) node.castShadow = true;
+  });
+
+  //add a hitbox
+  const letterBoxCylinder = new CANNON.Cylinder(.18, .18, 1, 12);
+  const physics = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: letterBoxCylinder,
+    position: new CANNON.Vec3(xPos, yPos + .5, zPos)
+  });
+
+  return {model, physics};
+}
+
+export {loadLetterBox}
