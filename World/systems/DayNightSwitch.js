2d62a0699be283670d5c5b34aa6db2e424a4e9b3
Thibse <thibault.rey@outlook.com>
2022-11-20 00:48:37 +0100

    :recycle: daylight switch

--- World/systems/DayNightSwitch.js
+++ World/systems/DayNightSwitch.js
@@ -1,19 +1,16 @@
-class DayNightSwitch  {
+class DayNightSwitch {
     constructor() {
         this.switchables = [];
-        this.state = false;
 
-        window.toggleDayNight = () => {
-            if(this.state) {
-                this.switchables.forEach(e => {
-                    e.day();
-                })
-            } else {
-                this.switchables.forEach(e => {
-                    e.night();
-                })                
-            }
-            this.state = !this.state;
+        window.toggleDay = () => {
+            this.switchables.forEach(e => {
+                e.day();
+            })
+        }
+        window.toggleNight = () => {
+            this.switchables.forEach(e => {
+                e.night();
+            })
         }
     }
 }
